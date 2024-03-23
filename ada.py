01039fc5a3132798d0bcad1392500c80bba0ef84
lucianereaperez <162724754+lucianereaperez@users.noreply.github.com>
2024-03-23 11:59:33 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -3,5 +3,5 @@ def ada():
     last_name = "LoVeLAce"
     print(f"{first_name.lower()} {last_name.lower()}")
     print(f"{first_name.title()} {last_name.title()}")
-    print(f"{first_name.upper()}+{last_name.upper()}")
-    print("	""f{first_name.lower()} {last_name.lower()}")
+    print(f"{first_name.upper()} {last_name.upper()}")
+    print(f"\t{first_name.lower()} {last_name.lower()}")
