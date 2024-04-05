a653c7e77a0b80e60cd80bed76506609a5e62b15
lucianereaperez <162724754+lucianereaperez@users.noreply.github.com>
2024-04-04 22:40:55 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -5,4 +5,4 @@ def earth():
     result = (x < y)
     print(f"The result of {x} comes first in the dictionary than {y} is {result}.")
     print(f"The result of {y} comes first in the dictionary than {y} is {not result}.")
-    earth()
+earth()
